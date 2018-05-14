4c99586c1053d3e216761d8a3241f398c919c834
Weihe <weihewang2012@gmail.com>
2018-05-14 00:34:03 -0700

    fix test case

--- test_apps.py
+++ test_apps.py
@@ -22,14 +22,6 @@ def test_insert_single_word(app, input_str, expected):
     assert (node.prefix, node.isWord) == (expected, True)
 
 
-def test_search_with_empty_input(app):
-    # After insert via search and update, should return overall top results
-    app.search('probing')
-    app.update_top_results()
-    res = app.search("")
-    assert res[0] == 'probing'
-
-
 def test_search_with_space(app):
     # After insert a space, should return an empty list
     app.search(' ')
@@ -41,29 +33,16 @@ def test_search_query_single_word(app):
     # After insert via search and update, should return the word
     app.search('testing', from_adv_app=False)
     app.update_top_results()
-    res = app.search("")
+    res = app.search('testing')
     assert res[0] == 'testing'
 
 
 def test_search_query_sentence(app):
-    # After insert via search and update, should a list containing all words in the query
+    # After insert via search and update, should return the query
     app.search('this is a cool test')
     app.update_top_results()
-    search_results = set(app.search(""))
-    search_term = 'this is a cool test'.split()
-    for word in search_term:
-        assert word in search_results
-
-
-def test_search_prefix_chain_creation(app):
-    # insert a word and all prefix nodes should be correctly created
-    test_word = "Spectacular"
-    prefixes = [test_word[:i] for i in range(len(test_word))]
-    app.search(test_word)
-    node = app.root
-    for i in range(len(test_word)):
-        assert node.prefix == prefixes[i]
-        node = node.children[test_word[i]]
+    search_results = app.search('this is a cool test')
+    assert search_results[0] == 'this is a cool test'
 
 
 def test_representation_singleword(app):
