4235e3acf8316d5397bb47bcce4054ba4dd15b0d
Tim Wang <weihewang2012@gmail.com>
2019-09-16 17:08:12 -0700

    add default parameters for RedisManager init and structuralize Redis keys for search

--- iomanagers/redis_manager.py
+++ iomanagers/redis_manager.py
@@ -6,14 +6,16 @@ import redis
 
 
 class RedisManager:
-    def __init__(self, redis_host: str, redis_port: str, db_idx: int):
+    def __init__(self, redis_host: str = 'localhost', redis_port: str = '6379', db_idx: int = 0):
         # decode_responses equals True makes sure the response is composed of strings instead of bytes
         self.redis_client = redis.Redis(redis_host, redis_port, db_idx, decode_responses=True)
 
     # save search results as Redis list
     def cache_search_results(self, search_term: str, search_results: list) -> None:
-        self.redis_client.rpush(search_term, *search_results)
+        redis_key = "search_term:" + search_term
+        self.redis_client.rpush(redis_key, *search_results)
 
     # retrieve search results from Redis
     def get_search_results(self, search_term: str) -> list:
-        return self.redis_client.lrange(search_term, 0, -1)
+        redis_key = "search_term:" + search_term
+        return self.redis_client.lrange(redis_key, 0, -1)
